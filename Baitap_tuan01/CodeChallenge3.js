8566153b0187c2ec7c635556350bbfb27f9c82ec
Vu20020814 <nguyenvanvu20020814@gmail.com>
2023-09-08 17:35:13 +0700

    chanllenge 3 final

--- Baitap_tuan01/CodeChallenge3.js
+++ Baitap_tuan01/CodeChallenge3.js
@@ -1,44 +1,45 @@
-const data1 = [{
-        team: "Dolphins",
-        score: [96, 108, 89]
-    },
-    {
-        team: "Koalas",
-        score: [88, 91, 110]
-    }
-]
-const dataBonus1 = [{
-        team: "Dolphins",
-        score: [97, 112, 101]
-    },
-    {
-        team: "Koalas",
-        score: [109, 95, 123]
-    }
-]
-const dataBonus2 = [{
-        team: "Dolphins",
-        score: [97, 112, 101]
-    },
-    {
-        team: "Koalas",
-        score: [109, 95, 106]
-    }
-]
-const AvgFn = function(sroce) {
-        const data = sroce.reduce(function(acc, value) {
-            return acc + value
-        }, 0) / 3
-        console.log(data)
-        return data
-    }
-    // Cau 2
-if (AvgFn(data1[0].score) > AvgFn(data1[1].score)) {
-    console.log(`Team Dolphins have avg score greater than Team Koalas`)
-} else
-if (AvgFn(data1[1].score) > AvgFn(data1[0].score)) {
-    console.log(`Team Koalas  have avg score greater than Team Dolphins`)
-} else {
-    console.log(`Team Koalas  draw  Team Dolphins`)
+// Cau 1
+// Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+let scrorDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110) / 3;
+console.log(` AVG Score Team Dolphins :`, scrorDolphins);
+console.log(` AVG Score Team Koalas :`, scoreKoalas);
+///////////////////////////////////////////////////////
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+if (scrorDolphins > scoreKoalas) {
+  console.log("Team Dolphins win the game  ");
+} else if (scrorDolphins < scoreKoalas) {
+  console.log("Team Koalas win the game  ");
+} else if (scrorDolphins === scrorDolphins) {
+  console.log("Draw");
 }
-// Cau 3
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+scrorDolphins = (97 + 112 + 101) / 3;
+scoreKoalas = (109 + 95 + 123) / 3;
+console.log(` AVG Score Team Dolphins :`, scrorDolphins);
+console.log(` AVG Score Team Koalas :`, scoreKoalas);
+if (scrorDolphins > scoreKoalas && scrorDolphins >= 100) {
+  console.log("Team Dolphins win the game  ");
+} else if (scrorDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Team Koalas win the game  ");
+} else if (scrorDolphins === scrorDolphins) {
+  console.log("Draw");
+}
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+scrorDolphins = (97 + 112 + 101) / 3;
+scoreKoalas = (109 + 95 + 106) / 3;
+console.log(` AVG Score Team Dolphins :`, scrorDolphins);
+console.log(` AVG Score Team Koalas :`, scoreKoalas);
+if (scrorDolphins > scoreKoalas && scrorDolphins >= 100) {
+  console.log("Team Dolphins win the game  ");
+} else if (scrorDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Team Koalas win the game ");
+} else if (
+  scrorDolphins === scrorDolphins &&
+  scrorDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("Draw");
+}
